3d279e2f10fce78895ea5cc2267c4fd25b8e7775
wangchong <buyimoutianxia@126.com>
2019-12-17 21:14:43 +0800

    mybatis多数据源

---
+++ mybatisdemo/src/main/java/com/xyz/service/UserService.java
@@ -0,0 +1,25 @@
+package com.xyz.service;
+
+import com.xyz.entity.User;
+import com.xyz.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    public int add(User user) {
+        int ret = 0;
+        try {
+            ret = userMapper.addUser(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return ret;
+        }
+    }
+
+}
